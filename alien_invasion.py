fb1b25b3571e24e306fc5abfbf0e0246a7dff817
David Ornellas <ornellasd@gmail.com>
2018-01-21 12:39:00 -0800

    added ship controls/boundaries

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,11 +11,12 @@ def run_game():
     pygame.display.set_caption('Alien Invasion')
 
     # make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
     
     # main game loop
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)    
     
 run_game()
--- settings.py
+++ settings.py
@@ -6,3 +6,6 @@ class Settings():
         self.screen_width = 1920
         self.screen_height = 1080
         self.bg_color = (230, 230, 230)
+
+        # ship settings
+        self.ship_speed_factor = 7.5
