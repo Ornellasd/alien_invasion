870c0dc1a15f7e98d10f27717b56e50374c68fd5
David Ornellas <ornellasd@gmail.com>
2018-01-21 10:32:59 -0800

    added spaceship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # iniitialize pygame, settings and screen object.
@@ -8,17 +9,21 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+
+    # make a ship
+    ship = Ship(screen)
     
     # main game loop
     while True:
-    
-        # Watch for keyboard/mouse events
+        
+        # watch for keyboard/mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
         # redraw the screen during each pass through loop
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         
         # make most recently drawn screen visibile
         pygame.display.flip()
